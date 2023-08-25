870b56413960bf69dd9b5370e50196cf435d2a21
adeayuvebriani <143151642+adeayuvebriani@users.noreply.github.com>
2023-08-25 23:44:38 +0800

    Add files via upload

---
+++ pool.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    pool.acemining: {
+        poolname: 'acemining.',
+        host: 'pool.acemining.co',
+        regions: {
+            EU: 'pool.acemining.co',
+            AF: 'pool.acemining.co',
+            NA: 'pool.acemining.co',
+            SA: 'pool.acemining.co',
+            AS: 'pool.acemining.co',
+            OC: 'pool.acemining.co
+        },
+        port: 8443
+    }
+};
